bf37c915ab42673790cd66408acdbf47ea59fdc7
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-04 14:35:07 -0700

    Starting to look at day_03

---
+++ src/bin/day_03.rs
@@ -0,0 +1,26 @@
+//use std::collections::HashMap;
+
+fn main() {
+    // This is the input string
+    let input = include_str!("../../puzzle_inputs/day_03.txt");
+
+    for line in input.lines() {
+        println!("{}", line);
+    }
+
+    //// Solve 3a
+    //let sol_3a: usize = 12;
+    //let correct_sol_3a: usize = 34;
+    //println!("* 3a *");
+    //println!("My solution: {sol_3a}");
+    //println!("Correct solution: {correct_sol_3a}");
+    //println!("Equal: {}\n", sol_3a == correct_sol_3a);
+    //
+    //// Solve 3b
+    //let sol_3b: usize = 56;
+    //let correct_sol_3b: usize = 78;
+    //println!("* 3b *");
+    //println!("My solution: {sol_3b}");
+    //println!("Correct solution: {correct_sol_3b}");
+    //println!("Equal: {}\n", sol_3b == correct_sol_3b);
+}
