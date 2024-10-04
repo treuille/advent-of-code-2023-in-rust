0305cbb87dd24ea3dc9bafff7a97d8ac01f7bac0
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-04 13:20:13 -0700

    Solved 2a

--- src/bin/day_02.rs
+++ src/bin/day_02.rs
@@ -1,4 +1,4 @@
-//use std::{collections::HashMap, fmt::write};
+use std::collections::HashMap;
 
 //use std::fmt::write;
 
@@ -7,21 +7,55 @@ fn main() {
     let input = include_str!("../../puzzle_inputs/day_02.txt");
 
     println!("input length: {}", input.len());
-    println!("input:\n{}", input);
+    //println!("input:\n{}", input);
 
-    // Dummy solution to 2a
-    let sol_2a = 123;
-    let correct_sol_2a: usize = 456;
-    println!("* 2A *");
-    println!("My solution: {sol_2a}");
-    println!("Correct solution: {correct_sol_2a}");
-    println!("Equal: {}\n", sol_2a == correct_sol_2a);
+    let max_cubes: HashMap<&str, usize> = [("red", 12), ("green", 13), ("blue", 14)]
+        .into_iter()
+        .collect();
 
-    // Dummy solution to 2a
-    let sol_2b = 78;
-    let correct_sol_2b: usize = 92;
-    println!("* 2B *");
-    println!("My solution: {sol_2b}");
-    println!("Correct solution: {correct_sol_2b}");
-    println!("Equal: {}\n", sol_2b == correct_sol_2b);
+    #[allow(clippy::unnecessary_filter_map)]
+    let answer = input
+        .lines()
+        .zip(1..)
+        .filter_map(|(line, game_num)| {
+            //println!("line: {line}");
+            let (_game_str, subsets_str) = line.split_once(": ").unwrap();
+            //println!("game: \"{game_str}\"");
+            //println!("game_num: \"{game_num}\"");
+            //
+            //println!("subsets: \"{subsets_str}\"");
+            for subset in subsets_str.split(", ") {
+                //println!("subset: {subset}");
+                for reveal in subset.split("; ") {
+                    //println!("reveal: \"{reveal}\"");
+                    let (num_cubes, cube_color) = reveal.split_once(" ").unwrap();
+                    //println!("num_cubes: \"{num_cubes}\"");
+                    let num_cubes = num_cubes.parse::<usize>().unwrap();
+                    //println!("num_cubes: {num_cubes}");
+                    //println!("cube_color: \"{cube_color}\"");
+                    if num_cubes > max_cubes[cube_color] {
+                        return None;
+                    }
+                }
+            }
+            Some(game_num)
+        })
+        .sum::<usize>();
+
+    println!("answer: {}", answer);
+    //// Dummy solution to 2a
+    //let sol_2a = 123;
+    let _correct_sol_2a: usize = 2268;
+    //println!("* 2A *");
+    //println!("My solution: {sol_2a}");
+    //println!("Correct solution: {correct_sol_2a}");
+    //println!("Equal: {}\n", sol_2a == correct_sol_2a);
+    //
+    //// Dummy solution to 2a
+    //let sol_2b = 78;
+    //let correct_sol_2b: usize = 92;
+    //println!("* 2B *");
+    //println!("My solution: {sol_2b}");
+    //println!("Correct solution: {correct_sol_2b}");
+    //println!("Equal: {}\n", sol_2b == correct_sol_2b);
 }
