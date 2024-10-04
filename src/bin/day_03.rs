eb3c4322fed54d3aea81e614a9f8b8c7ba198a0d
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-04 16:36:37 -0700

    Cleaning up 3a and b

--- src/bin/day_03.rs
+++ src/bin/day_03.rs
@@ -1,69 +1,35 @@
 //use std::collections::HashMap;
 
-const _INCORRECT: usize = 549927;
+// Correct answers
+// part_sum:
+// gear_ratio_sum:
 
 fn main() {
     // Parse the input
     let input = include_str!("../../puzzle_inputs/day_03.txt");
     let (parts, symbols) = parse_input(input);
 
-    //for (i, part) in parts.iter().enumerate() {
-    //    if i < 10 || i > parts.len() - 10 {
-    //        println!("part {}: {:?}", i, part);
-    //    }
-    //}
-    //for (i, symbol) in symbols.iter().enumerate() {
-    //    if i < 10 || i > symbols.len() - 10 {
-    //        println!("symbol {}: {:?}", i, symbol);
-    //    }
-    //}
-
-    let mut part_sum = 0;
-    for part in parts.iter() {
-        for symbol in symbols.iter() {
-            if part.adjacent_to(symbol) {
-                part_sum += part.part_num;
-                //println!("{:?} adjacent to {:?}", part, symbol);
-                //println!("part_sum: {}", part_sum);
-                break;
-            }
-        }
-    }
-    println!("part_sum: {}", part_sum);
-
-    let mut gear_ratio_sum = 0;
-    for symbol in symbols.iter() {
-        let adjacent_parts: Vec<&Part> = parts
-            .iter()
-            .filter(|part| part.adjacent_to(symbol))
-            .collect();
-        if adjacent_parts.len() == 2 {
-            gear_ratio_sum += adjacent_parts[0].part_num * adjacent_parts[1].part_num;
-        }
-    }
-    println!("gear_ratio_sum: {}", gear_ratio_sum);
-
     // Now we have the parts and symbols, we can start solving the puzzle
 
-    //// Solve 3a
-    //let sol_3a: usize = 12;
-    //let correct_sol_3a: usize = 34;
-    //println!("* 3a *");
-    //println!("My solution: {sol_3a}");
-    //println!("Correct solution: {correct_sol_3a}");
-    //println!("Equal: {}\n", sol_3a == correct_sol_3a);
-    //
-    //// Solve 3b
-    //let sol_3b: usize = 56;
-    //let correct_sol_3b: usize = 78;
-    //println!("* 3b *");
-    //println!("My solution: {sol_3b}");
-    //println!("Correct solution: {correct_sol_3b}");
-    //println!("Equal: {}\n", sol_3b == correct_sol_3b);
+    // Solve 3a
+    let sol_3a: usize = solve_3a(&parts, &symbols);
+    let correct_sol_3a: usize = 546312;
+    println!("* 3a *");
+    println!("My solution: {sol_3a}");
+    println!("Correct solution: {correct_sol_3a}");
+    println!("Equal: {}\n", sol_3a == correct_sol_3a);
+
+    // Solve 3b
+    let sol_3b: usize = solve_3b(&parts, &symbols);
+    let correct_sol_3b: usize = 87449461;
+    println!("* 3b *");
+    println!("My solution: {sol_3b}");
+    println!("Correct solution: {correct_sol_3b}");
+    println!("Equal: {}\n", sol_3b == correct_sol_3b);
 }
 
 /// A part somehwere on the grid
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 struct Part {
     part_num: usize,
     x: usize,
@@ -71,8 +37,6 @@ struct Part {
 }
 
 /// A symbol on the grid
-#[allow(dead_code)]
-#[derive(Debug)]
 struct Symbol {
     x: usize,
     y: usize,
@@ -93,6 +57,35 @@ impl Part {
     }
 }
 
+fn solve_3a(parts: &[Part], symbols: &[Symbol]) -> usize {
+    let mut part_sum = 0;
+    for part in parts.iter() {
+        for symbol in symbols.iter() {
+            if part.adjacent_to(symbol) {
+                part_sum += part.part_num;
+                //println!("{:?} adjacent to {:?}", part, symbol);
+                //println!("part_sum: {}", part_sum);
+                break;
+            }
+        }
+    }
+    part_sum
+}
+
+fn solve_3b(parts: &[Part], symbols: &[Symbol]) -> usize {
+    let mut gear_ratio_sum = 0;
+    for symbol in symbols.iter() {
+        let adjacent_parts: Vec<&Part> = parts
+            .iter()
+            .filter(|part| part.adjacent_to(symbol))
+            .collect();
+        if adjacent_parts.len() == 2 {
+            gear_ratio_sum += adjacent_parts[0].part_num * adjacent_parts[1].part_num;
+        }
+    }
+    gear_ratio_sum
+}
+
 /// This is the state of the parser as we go through finding parts and symbols
 fn parse_input(input: &str) -> (Vec<Part>, Vec<Symbol>) {
     let mut parts: Vec<Part> = Vec::new();
@@ -109,13 +102,7 @@ fn parse_input(input: &str) -> (Vec<Part>, Vec<Symbol>) {
                     // Just finding a . is a no-op
                     continue;
                 }
-                (None, '0') => {
-                    unimplemented!("0 is not a valid part number prefix");
-                }
                 (None, '0'..='9') => {
-                    if c == '0' {
-                        unimplemented!("0 is not a valid part number prefix");
-                    }
                     // We are at the start of a new part
                     parsing_part = Some(Part {
                         part_num: c.to_digit(10).unwrap() as usize,
