0a0dcf9a41c2213119bbb6536a4f4e59a7e40f54
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-05 13:46:58 -0700

    Cleaned up 4a and b

--- src/bin/day_04.rs
+++ src/bin/day_04.rs
@@ -1,75 +1,42 @@
 use std::collections::{/*HashMap, */ HashSet};
 
 fn main() {
-    // Parse the input
+    // Parse the input, counting the number of matches per card
     let input = include_str!("../../puzzle_inputs/day_04.txt");
-    //let input = include_str!("../../puzzle_inputs/day_04_test.txt");
-    //println!("Input length: {}\n", input.len());
-    let cards = parse_input(input);
-    //println!("cards: {:?}", cards[0]);
-    let mut all_points = 0;
-    let mut copies: Vec<usize> = vec![1; cards.len()];
-    for (i, card) in cards.iter().enumerate() {
-        let winning_numbers = card
-            .winning_numbers
-            .intersection(&card.your_numbers)
-            .count();
-        let i_count: usize = copies[i];
-        for j in (i + 1)..(i + 1 + winning_numbers) {
-            copies[j] += i_count;
-        }
-        //println!("copies: {:?}", copies);
-        let points = winning_numbers.checked_sub(1).map(|x| 1 << x).unwrap_or(0);
-        all_points += points;
-    }
-    println!("all_points: {}", all_points);
-    println!("copies sum: {:?}", copies.iter().sum::<usize>());
-
-    //let (parts, symbols) = parse_input(input);
-    //
-    //// Solve 3a
-    //let sol_3a: usize = solve_3a(&parts, &symbols);
-    //let correct_sol_3a: usize = 546312;
-    //println!("* 3a *");
-    //println!("My solution: {sol_3a}");
-    //println!("Correct solution: {correct_sol_3a}");
-    //println!("Equal: {}\n", sol_3a == correct_sol_3a);
-    //
-    //// Solve 3b
-    //let sol_3b: usize = solve_3b(&parts, &symbols);
-    //let correct_sol_3b: usize = 87449461;
-    //println!("* 3b *");
-    //println!("My solution: {sol_3b}");
-    //println!("Correct solution: {correct_sol_3b}");
-    //println!("Equal: {}\n", sol_3b == correct_sol_3b);
-}
-
-#[allow(dead_code)]
-#[derive(Debug)]
-struct Card {
-    winning_numbers: HashSet<usize>,
-    your_numbers: HashSet<usize>,
-}
-
-fn parse_input(input: &str) -> Vec<Card> {
-    input
+    let matches_per_card: Vec<usize> = input
         .lines()
         .map(|line| {
             let card = line.split_once(": ").unwrap().1;
-            let (winning_numbers, your_numbers) = card.split_once(" | ").unwrap();
-            //println!("card: \"{card}\"");
-            //println!("winning_numbers: \"{winning_numbers}\"");
-            //println!("your_numbers: \"{your_numbers}\"");
-            Card {
-                winning_numbers: winning_numbers
-                    .split_whitespace()
-                    .map(|num| num.parse().unwrap())
-                    .collect(),
-                your_numbers: your_numbers
-                    .split_whitespace()
-                    .map(|num| num.parse().unwrap())
-                    .collect(),
-            }
+            let (winning_nums, your_nums) = card.split_once(" | ").unwrap();
+            let winning_nums: HashSet<&str> = winning_nums.split_whitespace().collect();
+            let your_nums: HashSet<&str> = your_nums.split_whitespace().collect();
+            winning_nums.intersection(&your_nums).count()
         })
-        .collect()
+        .collect();
+
+    // Acccumulate partial answers to 4a and 4b in `points` and `copies`
+    let mut points = 0;
+    let mut copies: Vec<usize> = vec![1; matches_per_card.len()];
+    for (i, matches) in matches_per_card.iter().enumerate() {
+        for j in (i + 1)..(i + 1 + matches) {
+            copies[j] += copies[i];
+        }
+        points += (1 << matches) >> 1;
+    }
+
+    // Solve 4a
+    let sol_4a: usize = points;
+    let correct_sol_4a: usize = 23028;
+    println!("* 4a *");
+    println!("My solution: {sol_4a}");
+    println!("Correct solution: {correct_sol_4a}");
+    println!("Equal: {}\n", sol_4a == correct_sol_4a);
+
+    // Solve 4b
+    let sol_4b: usize = copies.iter().sum();
+    let correct_sol_4b: usize = 9236992;
+    println!("* 4b *");
+    println!("My solution: {sol_4b}");
+    println!("Correct solution: {correct_sol_4b}");
+    println!("Equal: {}\n", sol_4b == correct_sol_4b);
 }
