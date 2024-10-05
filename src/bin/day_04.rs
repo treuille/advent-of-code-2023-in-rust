1d74b7ba6f2929ab72bd4d671dcf5d5daf2db20b
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-04 19:27:58 -0700

    Starting to work on day 4

---
+++ src/bin/day_04.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // Parse the input
+    let input = include_str!("../../puzzle_inputs/day_04.txt");
+    println!("Input length: {}\n", input.len());
+
+    //let (parts, symbols) = parse_input(input);
+    //
+    //// Solve 3a
+    //let sol_3a: usize = solve_3a(&parts, &symbols);
+    //let correct_sol_3a: usize = 546312;
+    //println!("* 3a *");
+    //println!("My solution: {sol_3a}");
+    //println!("Correct solution: {correct_sol_3a}");
+    //println!("Equal: {}\n", sol_3a == correct_sol_3a);
+    //
+    //// Solve 3b
+    //let sol_3b: usize = solve_3b(&parts, &symbols);
+    //let correct_sol_3b: usize = 87449461;
+    //println!("* 3b *");
+    //println!("My solution: {sol_3b}");
+    //println!("Correct solution: {correct_sol_3b}");
+    //println!("Equal: {}\n", sol_3b == correct_sol_3b);
+}
