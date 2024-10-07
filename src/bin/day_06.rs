bfaa19fecc7e689f7b1653094eb6ceb946e82eba
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-06 18:56:03 -0700

    Started working on day 6

---
+++ src/bin/day_06.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let test_input_times: [usize; 3] = [7, 15, 30];
+    let test_input_dists: [usize; 3] = [9, 40, 200];
+
+    let input_times: &[usize] = &test_input_times;
+    let input_dists: &[usize] = &test_input_dists;
+
+    println!("input_times: {:?}", input_times);
+    println!("input_dists: {:?}", input_dists);
+
+    //// Solve 5a
+    //let sol_5a: usize = 12;
+    //let correct_sol_5a: usize = 32;
+    //println!("* 5a *");
+    //println!("My solution: {sol_5a}");
+    //println!("Correct solution: {correct_sol_5a}");
+    //println!("Equal: {}\n", sol_5a == correct_sol_5a);
+    //
+    //// Solve 5b
+    //let sol_5b: usize = 56;
+    //let correct_sol_5b: usize = 79;
+    //println!("* 5b *");
+    //println!("My solution: {sol_5b}");
+    //println!("Correct solution: {correct_sol_5b}");
+    //println!("Equal: {}\n", sol_5b == correct_sol_5b);
+}
