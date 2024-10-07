4f348598ec75b3de828d7f498e17baf270e213fd
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-07 10:38:09 -0700

    Solved 7a with Hand = [u8;5]

--- src/bin/day_07.rs
+++ src/bin/day_07.rs
@@ -1,56 +1,45 @@
 use std::cmp::Ordering;
-use std::fmt;
 
-// Correct answer A: 256448566
+/// A hande is a length-5 array of cards
+type Hand = [u8; 5];
 
-// Todo:
-// - Hand = [u8; 5]
-// - Move everything over
+/// A Puzzle is a list of hands and their bids
+type Puzzle = Vec<(Hand, usize)>;
 
 fn main() {
     //// Parse the input, counting the number of matches per card
     let input = include_str!("../../puzzle_inputs/day_07.txt");
     //let input = include_str!("../../puzzle_inputs/day_07_test.txt");
-    let mut input: Vec<(Hand, usize)> = parse_input(input.lines());
+    let mut input: Puzzle = parse_input(input.lines());
 
     //println!("* Input *");
     //for (hand, bid) in input.iter() {
     //    println!(
     //        "Hand: {}, Type: {:?}, Bid: {}",
-    //        hand,
-    //        HandType::from_hand(hand),
-    //        bid
-    //    );
-    //}
-
-    input.sort();
-
-    //println!("* Sorted Input *");
-    //for (hand, bid) in input.iter() {
-    //    println!(
-    //        "Hand: {}, Type: {:?}, Bid: {}",
-    //        hand,
+    //        to_string(hand),
     //        HandType::from_hand(hand),
     //        bid
     //    );
     //}
 
     // Solve 7a
-    let answer: usize = input
-        .iter()
-        .zip(1..)
-        .map(|((_hand, bid), order)| bid * order)
-        .sum();
-    println!("Answer: {}", answer);
+    let sol_7a: usize = solve(&mut input);
+    let correct_sol_7a: usize = 256448566;
+    println!("* 7a *");
+    println!("My solution: {sol_7a}");
+    println!("Correct solution: {correct_sol_7a}");
+    println!("Equal: {}\n", sol_7a == correct_sol_7a);
 
-    //// Solve 7a
-    //let sol_7a: usize = 12;
-    //let correct_sol_7a: usize = 32;
-    //println!("* 7a *");
-    //println!("My solution: {sol_7a}");
-    //println!("Correct solution: {correct_sol_7a}");
-    //println!("Equal: {}\n", sol_7a == correct_sol_7a);
-    //
+    //println!("* Sorted Input *");
+    //for (hand, bid) in input.iter() {
+    //    println!(
+    //        "Hand: {}, Type: {:?}, Bid: {}",
+    //        to_string(hand),
+    //        HandType::from_hand(hand),
+    //        bid
+    //    );
+    //}
+
     //// Solve 7b
     //let sol_7b: usize = 56;
     //let correct_sol_7b: usize = 79;
@@ -60,38 +49,45 @@ fn main() {
     //println!("Equal: {}\n", sol_7b == correct_sol_7b);
 }
 
-#[derive(Eq, Clone, Copy)]
-struct Card(char);
+fn solve(input: &mut Puzzle) -> usize {
+    input.sort_unstable_by(cmp_hand_and_bid);
 
-impl Card {
-    fn val(&self) -> usize {
-        match self.0 {
+    input
+        .iter()
+        .zip(1..)
+        .map(|((_hand, bid), order)| bid * order)
+        .sum()
+}
+
+fn to_hand(s: &str) -> Hand {
+    let hand: Vec<u8> = s
+        .chars()
+        .map(|c| match c {
             'T' => 10,
             'J' => 11,
             'Q' => 12,
             'K' => 13,
             'A' => 14,
-            c => c.to_digit(10).unwrap() as usize,
-        }
-    }
+            c => c.to_digit(10).unwrap() as u8,
+        })
+        .collect();
+    [hand[0], hand[1], hand[2], hand[3], hand[4]]
 }
 
-impl PartialEq for Card {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0
-    }
-}
-
-impl PartialOrd for Card {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Card {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.val().cmp(&other.val())
-    }
+fn to_string(hand: &Hand) -> String {
+    hand.iter()
+        .map(|c| {
+            match c {
+                0 => '*', // <- 0 means Joker
+                10 => 'T',
+                11 => 'J', // <- 11 means Jack
+                12 => 'Q',
+                13 => 'K',
+                14 => 'A',
+                c => c.to_string().chars().next().unwrap(),
+            }
+        })
+        .collect()
 }
 
 #[derive(Eq, Ord, PartialEq, PartialOrd, Debug)]
@@ -124,8 +120,8 @@ enum HandType {
 impl HandType {
     fn from_hand(hand: &Hand) -> Self {
         let mut counts: [usize; 15] = [0; 15];
-        for card in hand.0.iter() {
-            counts[card.val()] += 1;
+        for card in hand.iter() {
+            counts[*card as usize] += 1;
         }
 
         let mut pairs = 0;
@@ -140,7 +136,7 @@ impl HandType {
                 4 => quads += 1,
                 5 => quints += 1,
                 _ => {
-                    println!("Invalid hand: {}", hand);
+                    println!("Invalid hand: {}", to_string(hand));
                     println!("Counts: {:?}", counts);
                     println!("Pairs: {}", pairs);
                     println!("Triples: {}", triples);
@@ -159,61 +155,43 @@ impl HandType {
             (0, 0, 1, 0) => HandType::FourOfAKind,
             (0, 0, 0, 1) => HandType::FiveOfAKind,
             (1, 1, 0, 0) => HandType::FullHouse,
-            _ => panic!("Invalid hand: {}", hand),
+            _ => panic!("Invalid hand: {}", to_string(hand)),
         }
     }
 }
 
-#[derive(Eq)]
-struct Hand([Card; 5]);
-
-impl PartialEq for Hand {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0
+fn cmp_hand_and_bid((h1, b1): &(Hand, usize), (h2, b2): &(Hand, usize)) -> Ordering {
+    let ord = HandType::from_hand(h1).cmp(&HandType::from_hand(h2));
+    if ord != Ordering::Equal {
+        return ord;
     }
+
+    let ord = h1.cmp(h2);
+    if ord != Ordering::Equal {
+        return ord;
+    }
+
+    b1.cmp(b2)
+    //
+    //match
+    //    Ordering::Equal => {
+    //        for (card, other_card) in self.0.iter().zip(other.0.iter()) {
+    //            match card.cmp(other_card) {
+    //                Ordering::Equal => continue,
+    //                x => return x,
+    //            }
+    //        }
+    //        Ordering::Equal
+    //    }
+    //    x => x,
+    //}
 }
 
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match HandType::from_hand(self).cmp(&HandType::from_hand(other)) {
-            Ordering::Equal => {
-                for (card, other_card) in self.0.iter().zip(other.0.iter()) {
-                    match card.cmp(other_card) {
-                        Ordering::Equal => continue,
-                        x => return x,
-                    }
-                }
-                Ordering::Equal
-            }
-            x => x,
-        }
-    }
-}
-
-impl fmt::Display for Hand {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}{}{}{}{}",
-            self.0[0].0, self.0[1].0, self.0[2].0, self.0[3].0, self.0[4].0
-        )
-    }
-}
-
-fn parse_input(lines: impl Iterator<Item = &'static str>) -> Vec<(Hand, usize)> {
+fn parse_input(lines: impl Iterator<Item = &'static str>) -> Puzzle {
     lines
         .map(|line| {
             let (hand, bid) = line.split_once(' ').unwrap();
-            let hand: Vec<Card> = hand.chars().map(Card).collect();
-            let hand = Hand([hand[0], hand[1], hand[2], hand[3], hand[4]]);
-            let bid = bid.parse().unwrap();
-            (hand, bid)
+            (to_hand(hand), bid.parse().unwrap())
         })
         .collect()
 }
