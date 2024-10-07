432dea1722364a383308972bfe792d59d50ed078
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-06 20:47:26 -0700

    Solved and cleaned up day 6

--- src/bin/day_06.rs
+++ src/bin/day_06.rs
@@ -1,59 +1,34 @@
 fn main() {
-    let test_input_times: [usize; 3] = [7, 15, 30];
-    let test_input_dists: [usize; 3] = [9, 40, 200];
+    // Solve 5a
+    let sol_5a: usize = solve(&[40, 82, 84, 92], &[233, 1011, 1110, 1487]);
+    let correct_sol_5a: usize = 3316275;
+    println!("* 5a *");
+    println!("My solution: {sol_5a}");
+    println!("Correct solution: {correct_sol_5a}");
+    println!("Equal: {}\n", sol_5a == correct_sol_5a);
 
-    let input_times: &[usize] = &test_input_times;
-    let input_dists: &[usize] = &test_input_dists;
+    // Solve 5b
+    let sol_5b: usize = solve(&[40828492], &[233101111101487]);
+    let correct_sol_5b: usize = 27102791;
+    println!("* 5b *");
+    println!("My solution: {sol_5b}");
+    println!("Correct solution: {correct_sol_5b}");
+    println!("Equal: {}\n", sol_5b == correct_sol_5b);
+}
 
-    println!("input_times: {:?}", input_times);
-    println!("input_dists: {:?}", input_dists);
-
-    // This is the math
-    // s : seconds waited
-    // t - s : time to race
-    // s * (t - s) = s * t - s ^ 2 : distance raced
-    // t - 2 * s = 0 : distance maximum
-    // s = t / 2 : distance maximum
-    // m : distance to beat
-    // s * t - s ^ 2 - m = 0 : solutions to beat minimum distance
-    //   s = (t - sqrt(t ^ 2 - 4 * m)) / 2
-    //   A = -1
-    //   B = t
-    //   C = -m
-    //   s = (-B - sqrt(B ^ 2 - 4 * A * C)) / 2 * A
-    //   s = (-t - sqrt(t ^ 2 - 4 * m)) / -2
-    //   s = (t +- sqrt(t ^ 2 - 4 * m)) / 2
-
-    // Solve 1
-    for (&t, &m) in input_times.iter().zip(input_dists.iter()) {
-        let t_f64: f64 = t as f64;
-        let delta_squared_f64: f64 = (t * t - 4 * m) as f64;
-        assert!(delta_squared_f64 >= 0.0);
-        let delta_f64: f64 = delta_squared_f64.sqrt();
-        let min_f64 = ((t_f64 - delta_f64) / 2.0).ceil().max(0.0);
-        let max_f64 = ((t_f64 + delta_f64) / 2.0).floor().min(t_f64);
-        let min: usize = min_f64 as usize;
-        let max: usize = max_f64 as usize;
-        let solns = max - min + 1;
-        println!(
-            "t: {}, m: {}, min: {}, max: {}, solns: {}",
-            t, m, min, max, solns,
-        );
-    }
-
-    //// Solve 5a
-    //let sol_5a: usize = 12;
-    //let correct_sol_5a: usize = 32;
-    //println!("* 5a *");
-    //println!("My solution: {sol_5a}");
-    //println!("Correct solution: {correct_sol_5a}");
-    //println!("Equal: {}\n", sol_5a == correct_sol_5a);
-    //
-    //// Solve 5b
-    //let sol_5b: usize = 56;
-    //let correct_sol_5b: usize = 79;
-    //println!("* 5b *");
-    //println!("My solution: {sol_5b}");
-    //println!("Correct solution: {correct_sol_5b}");
-    //println!("Equal: {}\n", sol_5b == correct_sol_5b);
+fn solve(times: &[usize], dists: &[usize]) -> usize {
+    let epsilon: f64 = 1e-10;
+    times
+        .iter()
+        .zip(dists.iter())
+        .map(|(&time, &dist)| {
+            let t_f64: f64 = time as f64;
+            let delta_f64: f64 = ((time * time - 4 * dist) as f64).sqrt();
+            let min_f64 = ((t_f64 - delta_f64) / 2.0 + epsilon).ceil().max(0.0);
+            let max_f64 = ((t_f64 + delta_f64) / 2.0 - epsilon).floor().min(t_f64);
+            let min_usize: usize = min_f64 as usize;
+            let max_usize: usize = max_f64 as usize;
+            max_usize - min_usize + 1 // the number of solutions
+        })
+        .product()
 }
