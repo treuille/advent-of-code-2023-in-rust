75e562daa9fb8212753822201616d9a5880f676b
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-10 15:38:42 -0700

    Cleaned up Day 8 a lot

--- src/bin/day_08.rs
+++ src/bin/day_08.rs
@@ -6,10 +6,10 @@ use std::collections::{HashMap, HashSet};
 fn main() {
     // Parse the input, counting the number of matches per card
     let input = include_str!("../../puzzle_inputs/day_08.txt");
-    let (instructions, map) = parse_input(input);
+    let puzzle = Puzzle::new(input);
 
     // Solve 8a
-    let sol_8a = solve_8a(&instructions, &map);
+    let sol_8a = solve_8a(&puzzle);
     let correct_sol_8a: usize = 15989;
     println!("* 8a *");
     println!("My solution: {sol_8a}");
@@ -17,148 +17,118 @@ fn main() {
     println!("Equal: {}\n", sol_8a == correct_sol_8a);
 
     // Solve 8b
-    let sol_8b: usize = solve_8b(&instructions, &map);
+    let sol_8b: usize = solve_8b(&puzzle);
     let correct_sol_8b: usize = 13830919117339;
     println!("* 8b *");
     println!("My solution: {sol_8b}");
     println!("Correct solution: {correct_sol_8b}");
     println!("Equal: {}\n", sol_8b == correct_sol_8b);
-    println!("Solved 8b");
 }
 
-fn solve_8a(instructions: &[Instruction], map: &Map) -> usize {
-    let initial_state = "AAA";
-    instructions
-        .iter()
-        .cycle()
-        .scan(initial_state, |node, instruction| {
-            if *node == "ZZZ" {
-                return None;
-            }
-            let next_node = match (instruction, map[node]) {
-                ('L', (s, _)) => s,
-                ('R', (_, s)) => s,
-                _ => panic!("Invalid instruction: {}", instruction),
-            };
-            *node = next_node;
-            Some(node.to_string())
-        })
-        .count()
+fn solve_8a(puzzle: &Puzzle) -> usize {
+    let initial_nodes = ["AAA"].into_iter().collect();
+    let terminal_nodes = ["ZZZ"].into_iter().collect();
+    puzzle.solve(&initial_nodes, &terminal_nodes)
 }
 
-fn solve_8b(instructions: &[Instruction], map: &Map) -> usize {
-    let initial_nodes: HashSet<Node> = map
+fn solve_8b(puzzle: &Puzzle) -> usize {
+    let initial_nodes: HashSet<&'static str> = puzzle
+        .map
         .keys()
         .filter(|node| node.ends_with("A"))
         .copied()
         .collect();
+    let terminal_nodes: HashSet<&'static str> = puzzle
+        .map
+        .keys()
+        .filter(|node| node.ends_with("Z"))
+        .copied()
+        .collect();
+    puzzle.solve(&initial_nodes, &terminal_nodes)
+}
 
-    // Least common multiple of two numbers
-    fn lcm(a: usize, b: usize) -> usize {
-        (a * b) / gcd(a, b)
+struct Puzzle {
+    instructions: Vec<char>,
+    map: HashMap<&'static str, (&'static str, &'static str)>,
+}
+impl Puzzle {
+    fn new(input: &'static str) -> Self {
+        let (instructions, map) = input.split_once("\n\n").unwrap();
+        let instructions: Vec<char> = instructions.chars().collect();
+
+        let pattern = r"(\w{3}) \= \((\w{3})\, (\w{3})\)";
+        let re = Regex::new(pattern).unwrap();
+        let map = parse_regex::parse_lines(re, map.trim())
+            .map(|(a, b, c)| (a, (b, c)))
+            .collect();
+
+        Self { instructions, map }
     }
 
-    let mut step = 0;
-    let mut step_len = 1;
-    for &node in initial_nodes.iter() {
-        let s = Sequence::new(node, instructions, map);
-        while !s.get(step).0.ends_with("Z") {
-            step += step_len;
-        }
-        step_len = lcm(step_len, s.cycle_len());
-    }
-    step
-}
+    /// Solve the puzzle
+    fn solve(
+        &self,
+        initial_nodes: &HashSet<&'static str>,
+        terminal_nodes: &HashSet<&'static str>,
+    ) -> usize {
+        // Least common multiple of two numbers
+        let lcm = |a, b| (a * b) / gcd(a, b);
 
-/// Instrucitons is a sequence of characters that can be either 'L' or 'R'
-type Instruction = char;
-
-/// A `Map` is a graph of nodes with two children, labeled 'L' or 'R'
-type Map = HashMap<&'static str, (&'static str, &'static str)>;
-
-type Node = &'static str;
-
-/// A `State` is both a node on the graph as well as an instruction pointer
-type State = (Node, usize);
-
-/// Turns `intial_node` into an sequence of states, cyclically
-/// following the `instructions` through the `map`
-fn state_iter<'a>(
-    initial_node: Node,
-    instructions: &'a [Instruction],
-    map: &'a Map,
-) -> impl Iterator<Item = State> + 'a {
-    let initial_state: State = (initial_node, 0);
-    std::iter::successors(Some(initial_state), |(node, instr_ptr)| {
-        let instruction = instructions.get(*instr_ptr).unwrap();
-        let instr_ptr = (instr_ptr + 1) % instructions.len();
-        match instruction {
-            'L' => Some((map[node].0, instr_ptr)),
-            'R' => Some((map[node].1, instr_ptr)),
-            _ => panic!("Invalid instruction: {}", instruction),
-        }
-    })
-}
-
-/// `Sequence` is a widget that can take any usize and compute the
-/// correspoinding state or any index 00..
-#[allow(dead_code)]
-#[derive(Debug)]
-struct Sequence {
-    cycle_start: usize,
-    states: Vec<State>,
-}
-
-#[allow(dead_code, unused_variables, unreachable_code)]
-impl Sequence {
-    #[allow(unused_mut)]
-    fn new(initial_node: Node, instructions: &[Instruction], map: &Map) -> Self {
-        // Our first state starts at the first instruction
-        let initial_state: State = (initial_node, 00);
-
-        // Create an endless iterator of states
-        let mut state_iter = state_iter(initial_node, instructions, map);
-
-        // Traverse this iterator until a cycle is found
-        let mut states: Vec<State> = Vec::new();
-        let mut visited_nodes: HashMap<State, usize> = HashMap::new();
-        loop {
-            let state = state_iter.next().unwrap();
-            if let Some(&cycle_start) = visited_nodes.get(&state) {
-                return Sequence {
-                    cycle_start,
-                    states,
-                };
-            } else {
-                states.push(state);
-                visited_nodes.insert(state, visited_nodes.len());
+        let mut step = 0;
+        let mut step_len = 1;
+        for &node in initial_nodes.iter() {
+            let path = self.path_from(node);
+            while !terminal_nodes.contains(path.at_step(step)) {
+                step += step_len;
             }
+            step_len = lcm(step_len, path.cycle_len());
         }
+        step
     }
 
-    fn get(&self, index: usize) -> State {
+    /// Construct a path from an initial node by following the instructions
+    fn path_from(&self, initial_node: &'static str) -> Path {
+        // Traverse this iterator until a cycle is found
+        let mut node = initial_node;
+        let mut instr_ptr = 0;
+        let mut nodes = Vec::new();
+        let mut visited_states = HashMap::new();
+        loop {
+            let state = (node, instr_ptr);
+            if let Some(&cycle_start) = visited_states.get(&state) {
+                return Path { cycle_start, nodes };
+            }
+            nodes.push(node);
+            visited_states.insert(state, visited_states.len());
+            node = match self.instructions.get(instr_ptr).unwrap() {
+                'L' => self.map[node].0,
+                'R' => self.map[node].1,
+                instruction => panic!("Invalid instruction: {}", instruction),
+            };
+            instr_ptr = (instr_ptr + 1) % self.instructions.len();
+        }
+    }
+}
+
+/// A `Path` turns an initial node into a sequence of nodes by following
+/// the instructions. Nodes can be computed by index in costant time
+/// using Path::at_step(index).
+struct Path {
+    cycle_start: usize,
+    nodes: Vec<&'static str>,
+}
+
+impl Path {
+    fn at_step(&self, index: usize) -> &'static str {
         let index = index
             .checked_sub(self.cycle_start)
             .map_or(index, |rel_index| {
                 self.cycle_start + rel_index % self.cycle_len()
             });
-        self.states[index]
+        self.nodes[index]
     }
 
     fn cycle_len(&self) -> usize {
-        self.states.len() - self.cycle_start
+        self.nodes.len() - self.cycle_start
     }
-}
-
-fn parse_input(input: &'static str) -> (Vec<Instruction>, Map) {
-    let (instructions, map) = input.split_once("\n\n").unwrap();
-    let instructions: Vec<Instruction> = instructions.chars().collect();
-
-    let pattern = r"(\w{3}) \= \((\w{3})\, (\w{3})\)";
-    let re = Regex::new(pattern).unwrap();
-    let map: Map = parse_regex::parse_lines(re, map.trim())
-        .map(|(a, b, c)| (a, (b, c)))
-        .collect();
-
-    (instructions, map)
 }
