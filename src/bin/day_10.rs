950713e062a60c4656d5a96bf8ed15c93c36224a
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-11 09:37:26 -0700

    Added a print_grid(..) fn to day_10.rs

--- src/bin/day_10.rs
+++ src/bin/day_10.rs
@@ -1,22 +1,16 @@
 use advent_of_code_2023_in_rust::grid::parse_char_grid;
+use ndarray::Array2;
 
 fn main() {
     // Parse the input, counting the number of matches per card
     let input = include_str!("../../puzzle_inputs/day_10_test.txt");
     println!("input len: {}", input.len());
     println!("input:\n{}", input);
-    let input = input
-        .trim()
-        .lines()
-        .map(|line| line.chars().skip(1).collect::<String>())
-        .collect::<Vec<_>>()
-        .join("\n");
-    println!("input len: {}", input.len());
-    println!("input:\n{}", input);
 
     let ident = |c: char| c;
     let puzzle = parse_char_grid(&input, ident); // Solve 10a
-    println!("puuzle input:\n{:?}", puzzle);
+    println!("puzzle input:\n{:?}", puzzle);
+    print_grid(&puzzle);
 
     //let sol_10a: usize = 12;
     //let correct_sol_10a: usize = 32;
@@ -32,4 +26,11 @@ fn main() {
     //println!("My solution: {sol_10b}");
     //println!("Correct solution: {correct_sol_10b}");
     //println!("Equal: {}\n", sol_10b == correct_sol_10b);
+}
+
+fn print_grid(grid: &Array2<char>) {
+    grid.rows().into_iter().for_each(|row| {
+        row.iter().for_each(|cell| print!("{}", cell));
+        println!();
+    });
 }
