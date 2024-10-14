5e7c588b4c608db67c268ab8a87a61f7ef495faf
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-14 10:58:14 -0700

    Have a solution to 12b, but it's running too slowly

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -1,35 +1,75 @@
+#[allow(unreachable_code, clippy::never_loop)]
 fn main() {
     // Parse the input, counting the number of matches per card
-    let input = include_str!("../../puzzle_inputs/day_12.txt");
-    //let input = include_str!("../../puzzle_inputs/day_12_test_1.txt");
-    println!("input len: {}", input.len());
-    println!("input:\n{}", input);
-    println!("parse_input(input): {:?}", parse_input(input));
+    //let input = include_str!("../../puzzle_inputs/day_12.txt");
+    let input = include_str!("../../puzzle_inputs/day_12_test_1.txt");
+    ////println!("input len: {}", input.len());
+    //println!("input:\n{}", input);
+    //println!("parse_input(input): {:?}", parse_input(input));
 
+    let puzzle = parse_input(input);
+
+    // Solve 12a
+    let sol_12a: usize = solve(&puzzle);
+    let correct_sol_12a: usize = 7622;
+    println!("* 12a *");
+    println!("My solution: {sol_12a}");
+    println!("Correct solution: {correct_sol_12a}");
+    println!("Equal: {}\n", sol_12a == correct_sol_12a);
+
+    let puzzle = increase_puzzle_size(puzzle);
+
+    //for (row, damaged_springs) in &puzzle {
+    //    println!("row: {:?}", row);
+    //    println!("damaged_springs: {:?}", damaged_springs);
+    //    println!();
+    //    break;
+    //}
+    //
+    //unimplemented!("early stop here");
+
+    // Solve 12b
+    let sol_12b: usize = solve(&puzzle);
+    let correct_sol_12b: usize = 79;
+    println!("* 12b *");
+    println!("My solution: {sol_12b}");
+    println!("Correct solution: {correct_sol_12b}");
+    println!("Equal: {}\n", sol_12b == correct_sol_12b);
+}
+
+fn solve(puzzle: &[(Vec<char>, Vec<usize>)]) -> usize {
     let mut total_arrangements = 0;
-    for (row, damaged_springs) in parse_input(input) {
+    for (row, damaged_springs) in puzzle {
         println!("row: {:?}", row);
         println!("damaged_springs: {:?}", damaged_springs);
-        let arrangements = count_arrangements(Some(row[0]), &row[1..], 0, &damaged_springs);
+        let arrangements = count_arrangements(Some(row[0]), &row[1..], 0, damaged_springs);
         println!("arrangements: {}\n", arrangements);
         total_arrangements += arrangements;
     }
     println!("total_arrangements: {}", total_arrangements);
-    //// Solve 12a
-    //let sol_12a: usize = 12;
-    //let correct_sol_12a: usize = 32;
-    //println!("* 12a *");
-    //println!("My solution: {sol_12a}");
-    //println!("Correct solution: {correct_sol_12a}");
-    //println!("Equal: {}\n", sol_12a == correct_sol_12a);
-    //
-    //// Solve 12b
-    //let sol_12b: usize = 56;
-    //let correct_sol_12b: usize = 79;
-    //println!("* 12b *");
-    //println!("My solution: {sol_12b}");
-    //println!("Correct solution: {correct_sol_12b}");
-    //println!("Equal: {}\n", sol_12b == correct_sol_12b);
+    total_arrangements
+}
+
+fn increase_puzzle_size(puzzle: Vec<(Vec<char>, Vec<usize>)>) -> Vec<(Vec<char>, Vec<usize>)> {
+    puzzle
+        .into_iter()
+        .map(|(row, damaged_springs)| {
+            let row_len = row.len();
+            let damaged_spring_len = damaged_springs.len();
+            let row = row
+                .into_iter()
+                .chain(['?'])
+                .cycle()
+                .take(row_len * 5 + 4)
+                .collect();
+            let damaged_springs = damaged_springs
+                .into_iter()
+                .cycle()
+                .take(damaged_spring_len * 5)
+                .collect();
+            (row, damaged_springs)
+        })
+        .collect()
 }
 
 #[allow(unreachable_code)]
