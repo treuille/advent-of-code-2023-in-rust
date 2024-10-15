9ba5801bc545ca5cf1ceccd1830062bd8d5f5fc4
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-14 18:51:12 -0700

    Tried another search algorithm which also failed

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -1,3 +1,6 @@
+#[allow(unused_imports)]
+use std::path::PrefixComponent;
+
 #[allow(unreachable_code, clippy::never_loop)]
 fn main() {
     // Parse the input, counting the number of matches per card
@@ -5,17 +8,18 @@ fn main() {
     //let input = include_str!("../../puzzle_inputs/day_12_test_1.txt");
 
     // Solve 12a
-    let puzzle = parse_input(input);
-    let sol_12a: usize = solve(&puzzle);
-    let correct_sol_12a: usize = 7622;
+    let mut puzzle = parse_input(input);
+    let sol_12a: usize = solve(&mut puzzle);
+    //let correct_sol_12a: usize = 7622;
+    let correct_sol_12a: usize = 21;
     println!("* 12a *");
     println!("My solution: {sol_12a}");
     println!("Correct solution: {correct_sol_12a}");
     println!("Equal: {}\n", sol_12a == correct_sol_12a);
 
     // Solve 12b
-    let puzzle = increase_puzzle_size(puzzle);
-    let sol_12b: usize = solve(&puzzle);
+    let mut puzzle = increase_puzzle_size(puzzle);
+    let sol_12b: usize = solve(&mut puzzle);
     let correct_sol_12b: usize = 525152;
     println!("* 12b *");
     println!("My solution: {sol_12b}");
@@ -23,17 +27,17 @@ fn main() {
     println!("Equal: {}\n", sol_12b == correct_sol_12b);
 }
 
-fn solve(puzzle: &[(Vec<char>, Vec<usize>)]) -> usize {
+fn solve(puzzle: &mut [(Vec<char>, Vec<usize>)]) -> usize {
     let mut total_arrangements = 0;
     for (row, damaged_springs) in puzzle {
         println!("row: {:?}", row);
-        println!("damaged_springs: {:?}", damaged_springs);
-        println!();
-        let arrangements = count_arrangements(Some(row[0]), &row[1..], 0, damaged_springs);
+        println!("damaged_springs: {:?}\n", damaged_springs);
+        let arrangements = count_arrangements(row, damaged_springs);
         println!("arrangements: {}\n", arrangements);
         total_arrangements += arrangements;
+        //panic!("Early exit");
     }
-    println!("total_arrangements: {}", total_arrangements);
+    //println!("total_arrangements: {}", total_arrangements);
     total_arrangements
 }
 
@@ -59,83 +63,90 @@ fn increase_puzzle_size(puzzle: Vec<(Vec<char>, Vec<usize>)>) -> Vec<(Vec<char>,
         .collect()
 }
 
-#[allow(unreachable_code)]
-fn count_arrangements(
-    first_spring: Option<char>,
-    rest_of_springs: &[char],
-    n_contiguous: usize,
-    damaged_springs: &[usize],
-) -> usize {
-    if *damaged_springs.first().unwrap_or(&0) < n_contiguous {
-        // Early return if we've already found too many contiguous damaged springs
-        return 0;
-    } else if n_contiguous == 0 {
-        // 14 would mean that we have 14 remaining springs to set
-        let springs_we_have_left: usize =
-            first_spring.map(|_| 1).unwrap_or(0) + rest_of_springs.len();
-
-        // 15 would mean that we need at least 15 more springs
-        // e.g. [4,3,1] -> ###.###.#
-        //let min_springs_we_need = damaged_springs.iter().sum::<usize>() + damaged_springs.len() - 1;
-        //let min_springs_we_need = damaged_springs.iter().sum::<usize>() + damaged_springs.len() - 1;
-        let min_springs_we_need: usize = damaged_springs.iter().sum();
-
-        if min_springs_we_need > springs_we_have_left {
-            //// TODO: Add early return explanation
-            //println!("first_spring: {:?}", first_spring);
-            //println!("rest_of_springs: {:?}", rest_of_springs);
-            //println!("rest_of_springs.len(): {}", rest_of_springs.len());
-            //println!("springs_we_have_left: {}", springs_we_have_left);
-            //println!();
-            //println!("damaged_springs: {:?}", damaged_springs);
-            //println!("min_springs_we_need: {}", min_springs_we_need);
-            //panic!("Early return A");
-            return 0;
+#[allow(unreachable_code, unused_variables, clippy::never_loop)]
+fn count_arrangements(mut row: &mut [char], mut damaged_springs: &[usize]) -> usize {
+    loop {
+        match row.first() {
+            Some(&'.') => {
+                // Skip any operational springs
+                //println!("row: {:?}", row);
+                //println!("damaged_springs: {:?}", damaged_springs);
+                //println!("Test case A1\n");
+                row = &mut row[1..];
+            }
+            Some(&'#') => {
+                // Skip any contiguous damaged springs
+                if let Some(&first_damaged_spring) = damaged_springs.first() {
+                    if row.len() < first_damaged_spring {
+                        //println!("row: {:?}", row);
+                        //println!("damaged_springs: {:?}", damaged_springs);
+                        //println!("Test case A2 - NO SOLUTION\n");
+                        return 0;
+                    }
+                    if row[0..first_damaged_spring].iter().all(|&c| c != '.') {
+                        //println!("row: {:?}", row);
+                        //println!("damaged_springs: {:?}", damaged_springs);
+                        //println!("Test case A3\n");
+                        row = &mut row[first_damaged_spring..];
+                        damaged_springs = &damaged_springs[1..];
+                        match row.first() {
+                            Some(&'#') => return 0,
+                            Some(_) => row = &mut row[1..],
+                            None => (),
+                        }
+                    } else {
+                        //println!("row: {:?}", row);
+                        //println!("damaged_springs: {:?}", damaged_springs);
+                        //println!("Test case B - NO SOLUTION\n");
+                        return 0;
+                    }
+                } else if row.iter().all(|&c| c != '#') {
+                    // The rest of the row is consistant with no more damaged springs
+                    //println!("row: {:?}", row);
+                    //println!("damaged_springs: {:?}", damaged_springs);
+                    //panic!("Test case C - FOUND A SOLUTION\n");
+                    return 1;
+                } else {
+                    // The rest of the row is *inconsistant* with no more damaged springs
+                    //println!("row: {:?}", row);
+                    //println!("damaged_springs: {:?}", damaged_springs);
+                    //println!("Test case D - NO SOLUTION\n");
+                    return 0;
+                }
+            }
+            Some(&'?') => {
+                //println!("row: {:?}", row);
+                //println!("damaged_springs: {:?}", damaged_springs);
+                //println!("Test case G\n");
+                row[0] = '.';
+                let case_a = count_arrangements(row, damaged_springs);
+                row[0] = '#';
+                let case_b = count_arrangements(row, damaged_springs);
+                row[0] = '?';
+                return case_a + case_b;
+            }
+            None => {
+                #[allow(clippy::redundant_pattern_matching)]
+                if let Some(_) = damaged_springs.first() {
+                    // Cannot satisfy
+                    //println!("row: {:?}", row);
+                    //println!("damaged_springs: {:?}", damaged_springs);
+                    //println!("Test case E - NO SOLUTION\n");
+                    return 0;
+                } else {
+                    //println!("row: {:?}", row);
+                    //println!("damaged_springs: {:?}", damaged_springs);
+                    //println!("Test case F - FOUND A SOLUTION\n");
+                    return 1;
+                }
+                // no springs left
+                unimplemented!("no springs left");
+            }
+            Some(&c) => unimplemented!("unexpected character: {}", c),
         }
     }
-    let (next_spring, subsequent_springs): (Option<char>, &[char]) = rest_of_springs
-        .split_first()
-        .map_or((None, &[]), |(first_spring, rest_of_springs)| {
-            (Some(*first_spring), rest_of_springs)
-        });
-
-    let matches_next_damaged_spring = Some(&n_contiguous) == damaged_springs.first();
-    match (
-        first_spring,
-        n_contiguous,
-        matches_next_damaged_spring,
-        damaged_springs.len(),
-    ) {
-        (Some('.'), 0, _, _) => {
-            count_arrangements(next_spring, subsequent_springs, 0, damaged_springs)
-        }
-        (Some('.'), _, true, _) => {
-            count_arrangements(next_spring, subsequent_springs, 0, &damaged_springs[1..])
-        }
-        (Some('.'), _, _, _) => 0,
-        (Some('#'), _, _, _) => count_arrangements(
-            next_spring,
-            subsequent_springs,
-            n_contiguous + 1,
-            damaged_springs,
-        ),
-        (Some('?'), _, true, _) => {
-            count_arrangements(Some('.'), rest_of_springs, n_contiguous, damaged_springs)
-        }
-        (Some('?'), _, _, 0) => {
-            count_arrangements(Some('.'), rest_of_springs, n_contiguous, damaged_springs)
-        }
-        (Some('?'), _, _, _) => {
-            count_arrangements(Some('.'), rest_of_springs, n_contiguous, damaged_springs)
-                + count_arrangements(Some('#'), rest_of_springs, n_contiguous, damaged_springs)
-        }
-        (None, 0, _, 0) => 1,
-        (None, 0, _, _) => 0,
-        (None, _, true, 1) => 1,
-        (None, _, true, 0) => 0,
-        (None, _, _, _) => 0,
-        _ => panic!("Invalid spring character: {:?}", first_spring),
-    }
+    //if let Some(&spring) = row.first() {
+    unimplemented!("count_arrangements");
 }
 
 fn parse_input(input: &'static str) -> Vec<(Vec<char>, Vec<usize>)> {
