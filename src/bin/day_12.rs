f88d07059795f91cc98724a21529502cba9b2c6f
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-16 11:48:29 -0700

    A litte more cleanup on day 12

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -1,5 +1,6 @@
 #![allow(clippy::obfuscated_if_else)]
 use cached::proc_macro::cached;
+use itertools::Itertools;
 use std::iter;
 
 /// A row of springs, eiher '.' (operational), '#' (damaged), or '?' (unknown)
@@ -47,16 +48,8 @@ fn increase_puzzle_size(puzzle: Vec<(Row, DamagedSprings)>) -> Vec<(Row, Damaged
     puzzle
         .into_iter()
         .map(|(row, damaged_springs)| {
-            let damaged_springs_len = damaged_springs.len();
-            let row = iter::repeat(row.clone())
-                .take(5)
-                .collect::<Vec<_>>()
-                .join("?");
-            let damaged_springs = damaged_springs
-                .into_iter()
-                .cycle()
-                .take(damaged_springs_len * 5)
-                .collect();
+            let row = iter::repeat(row).take(5).collect_vec().join("?");
+            let damaged_springs = iter::repeat(damaged_springs).take(5).flatten().collect();
             (row, damaged_springs)
         })
         .collect()
@@ -65,97 +58,80 @@ fn increase_puzzle_size(puzzle: Vec<(Row, DamagedSprings)>) -> Vec<(Row, Damaged
 #[cached]
 fn count_arrangements(row: Row, damaged_springs: DamagedSprings) -> usize {
     if damaged_springs.is_empty() {
-        match row.chars().all(|c| c != '#') {
-            true => 1,
-            false => 0,
-        }
+        return row.chars().all(|c| c != '#') as usize;
     } else if row.chars().filter(|&c| c != '.').count() < damaged_springs.iter().sum() {
-        0
-    } else {
-        // Split the damaged_springs into three parts damaged_springs[..,split,..]
-        let split = damaged_springs.len() / 2;
-        let left_damaged_springs = &damaged_springs[..split];
-        let right_damaged_springs = &damaged_springs[(split + 1)..];
+        return 0;
+    }
 
-        // The split occurs implicitly at a string of the form ".#####."
-        // where the "#"s match the length of split_len
-        // and each terminal "." depends on having a nonempty left or right split,
-        // respectively. These "."s force separation of the damaged springs.
-        let split_len = damaged_springs[split];
-        let split_str_len = split_len
-            + (!left_damaged_springs.is_empty() as usize)
-            + (!right_damaged_springs.is_empty() as usize);
+    // Split the damaged_springs into three parts damaged_springs[..,split,..]
+    let split = damaged_springs.len() / 2;
+    let left_damaged_springs = &damaged_springs[..split];
+    let right_damaged_springs = &damaged_springs[(split + 1)..];
 
-        // The pot_damaged vector is an acceleration structure
-        // that prevents us from scanning the entire row of damaged springs
-        let mut pot_damaged: Vec<usize> = Vec::with_capacity(row.len());
-        pot_damaged.extend(iter::once('.').chain(row.chars()).scan(0, |acc, c| {
-            *acc += (c == '.').then_some(0).unwrap_or(1);
-            Some(*acc)
-        }));
-        let max_pot_damaged = pot_damaged.last().unwrap();
-        let rds_sum: usize = right_damaged_springs.iter().sum();
+    // The split occurs implicitly at a string of the form ".#####."
+    // where the "#"s match the length of split_len
+    // and each terminal "." depends on having a nonempty left or right split,
+    // respectively. These "."s force separation of the damaged springs.
+    let split_spring = damaged_springs[split];
+    let split_str_len = split_spring
+        + (!left_damaged_springs.is_empty() as usize)
+        + (!right_damaged_springs.is_empty() as usize);
 
-        // Scan the row for possible splits
-        let mut total_arrangements: usize = 0;
-        for i in 0..=(row.len() - split_str_len) {
-            // Ensure the right split has suffciently many potential damaged springs.
-            // Since right_pot_damaged is decreasing, we can break early.
-            let right_pot_damaged = max_pot_damaged - pot_damaged[i + split_str_len];
-            if right_pot_damaged < rds_sum {
-                break;
-            }
+    // The pot_damaged vector is an acceleration structure
+    // that prevents us from scanning the entire row of damaged springs
+    let mut pot_damaged: Vec<usize> = Vec::with_capacity(row.len());
+    pot_damaged.extend(iter::once(0).chain(row.chars().scan(0, |acc, c| {
+        *acc += (c == '.').then_some(0).unwrap_or(1);
+        Some(*acc)
+    })));
+    let max_pot_damaged = pot_damaged.last().unwrap();
+    let rds_sum: usize = right_damaged_springs.iter().sum();
 
-            // Ensure the center split has suffciently many potential damaged springs
-            if pot_damaged[i + split_str_len] - pot_damaged[i] < split_len {
-                continue;
-            }
-
-            // Ensure that the center split is exctly valid
-            let left_empty = left_damaged_springs.is_empty();
-            let right_empty = right_damaged_springs.is_empty();
-            if !matches_row(&row[i..], split_len, left_empty, right_empty) {
-                continue;
-            }
-
-            // Count the number of arrangements on the left
-            let left_row = &row[..i];
-            let left_arrangements =
-                count_arrangements(left_row.to_string(), left_damaged_springs.to_vec());
-
-            // Early return if no arrangements are possible on the left
-            if left_arrangements == 0 {
-                continue;
-            }
-
-            // Count the number of arrangements on the right
-            let right_row = &row[(i + split_str_len)..];
-            let right_arrangements =
-                count_arrangements(right_row.to_string(), right_damaged_springs.to_vec());
-
-            // This is te key to the combinatoral speedup
-            total_arrangements += left_arrangements * right_arrangements
+    // Scan the row for possible splits
+    let mut total_arrangements: usize = 0;
+    for i in 0..=(row.len() - split_str_len) {
+        // Ensure the right split has suffciently many potential damaged springs.
+        // Since right_pot_damaged is decreasing, we can break early.
+        let right_pot_damaged = max_pot_damaged - pot_damaged[i + split_str_len];
+        if right_pot_damaged < rds_sum {
+            break;
         }
-        return total_arrangements;
-    }
-}
 
-// Implicitly matches the string ".#####." where the "#"s match the length of split_len
-fn matches_row(row: &str, split_len: usize, left_empty: bool, right_empty: bool) -> bool {
-    let mut row = row.chars();
-    if !left_empty && row.next() == Some('#') {
-        return false;
-    }
-    for _ in 0..split_len {
-        match row.next() {
-            Some('.') | None => return false,
-            _ => (),
+        // Ensure the center split has suffciently many potential damaged springs
+        if pot_damaged[i + split_str_len] - pot_damaged[i] < split_spring {
+            continue;
         }
+
+        // Ensure that the center split is exctly valid
+        let left_empty = left_damaged_springs.is_empty();
+        let right_empty = right_damaged_springs.is_empty();
+        let mut center_row = row.chars().skip(i);
+        if !left_empty && center_row.next() == Some('#')
+            || center_row.by_ref().take(split_spring).any(|c| c == '.')
+            || !right_empty && center_row.next() == Some('#')
+        {
+            continue;
+        }
+
+        // Count the number of arrangements on the left
+        let left_row = &row[..i];
+        let left_arrangements =
+            count_arrangements(left_row.to_string(), left_damaged_springs.to_vec());
+
+        // Early return if no arrangements are possible on the left
+        if left_arrangements == 0 {
+            continue;
+        }
+
+        // Count the number of arrangements on the right
+        let right_row = &row[(i + split_str_len)..];
+        let right_arrangements =
+            count_arrangements(right_row.to_string(), right_damaged_springs.to_vec());
+
+        // This is te key to the combinatoral speedup
+        total_arrangements += left_arrangements * right_arrangements
     }
-    if !right_empty && row.next() == Some('#') {
-        return false;
-    }
-    true
+    total_arrangements
 }
 
 fn parse_input(input: &'static str) -> Vec<(Row, DamagedSprings)> {
