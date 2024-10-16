34714fda93d16002fa119e0621c0865a08a4c2cd
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-15 22:15:25 -0700

    Solved 12b in 48s of computation.. can I make it faster?

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -16,8 +16,8 @@ fn main() {
     //unimplemented!("test_damaged_spring_regex");
 
     // Parse the input, counting the number of matches per card
-    //let input = include_str!("../../puzzle_inputs/day_12.txt");
-    let input = include_str!("../../puzzle_inputs/day_12_test_1.txt");
+    let input = include_str!("../../puzzle_inputs/day_12.txt");
+    //let input = include_str!("../../puzzle_inputs/day_12_test_1.txt");
 
     // Solve 12a
     let puzzle = parse_input(input);
@@ -29,14 +29,14 @@ fn main() {
     println!("Correct solution: {correct_sol_12a}");
     println!("Equal: {}\n", sol_12a == correct_sol_12a);
 
-    //// Solve 12b
-    //let puzzle = increase_puzzle_size(puzzle);
-    //let sol_12b: usize = solve(&puzzle);
-    //let correct_sol_12b: usize = 525152;
-    //println!("* 12b *");
-    //println!("My solution: {sol_12b}");
-    //println!("Correct solution: {correct_sol_12b}");
-    //println!("Equal: {}\n", sol_12b == correct_sol_12b);
+    // Solve 12b
+    let puzzle = increase_puzzle_size(puzzle);
+    let sol_12b: usize = solve(&puzzle);
+    let correct_sol_12b: usize = 525152;
+    println!("* 12b *");
+    println!("My solution: {sol_12b}");
+    println!("Correct solution: {correct_sol_12b}");
+    println!("Equal: {}\n", sol_12b == correct_sol_12b);
 }
 
 //#[cached]
@@ -63,7 +63,7 @@ fn main() {
 
 fn solve(puzzle: &[(Row, DamagedSprings)]) -> usize {
     let mut total_arrangements = 0;
-    for (row, damaged_springs) in puzzle {
+    for (row, damaged_springs) in puzzle.iter().skip(0) {
         println!("row: {:?}", row);
         println!("damaged_springs: {:?}\n", damaged_springs);
         let arrangements = count_arrangements(row, damaged_springs);
@@ -75,6 +75,7 @@ fn solve(puzzle: &[(Row, DamagedSprings)]) -> usize {
     total_arrangements
 }
 
+#[allow(dead_code)]
 fn increase_puzzle_size(puzzle: Vec<(Row, DamagedSprings)>) -> Vec<(Row, DamagedSprings)> {
     puzzle
         .into_iter()
@@ -98,13 +99,14 @@ fn increase_puzzle_size(puzzle: Vec<(Row, DamagedSprings)>) -> Vec<(Row, Damaged
         .collect()
 }
 
-fn rows_match(
-    row_1: impl IntoIterator<Item = char>,
-    row_2: impl IntoIterator<Item = char>,
-) -> bool {
+fn rows_match(row_1: &str, row_2: &str) -> bool {
+    //fn rows_match(
+    //    row_1: impl IntoIterator<Item = char>,
+    //    row_2: impl IntoIterator<Item = char>,
+    //) -> bool {
     row_1
-        .into_iter()
-        .zip(row_2)
+        .chars()
+        .zip(row_2.chars())
         .all(|(a, b)| a == b || a == '?' || b == '?')
     //let s1 = row_1.into_iter().collect::<String>();
     //let s2 = row_2.into_iter().collect::<String>();
@@ -120,69 +122,60 @@ fn rows_match(
     //answer
 }
 
+// TODO: Add a short curcuit basd on the sum(damaged_springs)
 #[allow(unused_variables, unreachable_code)]
-fn count_arrangements(mut row: &str, damaged_springs: &[usize]) -> usize {
-    // TODO: Add a short curcuit basd on the sum(damaged_springs)
-    while !row.is_empty() && &row[row.len() - 1..] == "." {
-        //println!("shortening: {:?} -> {:?}", row, &row[..(row.len() - 1)]);
-        row = &row[..(row.len() - 1)];
-    }
-    match damaged_springs.len() {
-        0 => unreachable!("Must have at least one damaged spring"),
-        1 => {
-            let damaged_spring = damaged_springs[0];
-            if row.len() < damaged_spring {
-                0
-            } else {
-                let beginning = ['.'].into_iter().cycle().take(row.len() - damaged_spring);
-                let end = ['#'].into_iter().cycle().take(damaged_spring);
-                match rows_match(beginning.chain(end), row.chars()) {
-                    false => 0,
-                    true => 1,
+fn count_arrangements(row: &str, damaged_springs: &[usize]) -> usize {
+    //println!(
+    //    "ENTRY\nrow: \"{}\"\ndamaged_springs: {:?}\n",
+    //    row, damaged_springs
+    //);
+
+    if damaged_springs.is_empty() {
+        if row.chars().all(|c| c != '#') {
+            //println!("CASE A1 - return 1\n");
+            1
+        } else {
+            //println!("CASE A2 - return 0\n");
+            0
+        }
+    } else if row.chars().filter(|&c| c != '.').count() < damaged_springs.iter().sum() {
+        //println!("CASE B - return 0\n");
+        0
+    } else {
+        //println!(
+        //    "CASE C\nrow: \"{}\"\ndamaged_springs: {:?}\n",
+        //    row, damaged_springs
+        //);
+        let split = damaged_springs.len() / 2;
+        assert!(split >= 0 && split < damaged_springs.len());
+        let left_damaged_springs = &damaged_springs[..split];
+        let right_damaged_springs = &damaged_springs[(split + 1)..];
+        let mut split_spring = "#".repeat(damaged_springs[split]);
+        if !left_damaged_springs.is_empty() {
+            split_spring.insert(0, '.');
+        }
+        if !right_damaged_springs.is_empty() {
+            split_spring.push('.');
+        }
+        let split_spring_len = split_spring.len();
+        //println!(
+        //    "CASE C\nleft: {:?} split: {:?} right: {:?}\n",
+        //    left_damaged_springs, split_spring, right_damaged_springs
+        //);
+        assert!(split_spring_len <= row.len());
+        (0..=row.len() - split_spring.len())
+            .filter(|&i| rows_match(&row[i..], &split_spring))
+            .map(|i| {
+                let left_row = &row[..i];
+                let right_row = &row[(i + split_spring_len)..];
+                let left_arrangements = count_arrangements(left_row, left_damaged_springs);
+                if left_arrangements == 0 {
+                    return 0;
                 }
-                //(0..(row.len() - damaged_spring + 1))
-                //    .filter(|&start| {
-                //        let beginning = ['.'].into_iter().cycle().take(start);
-                //        let middle = ['#'].into_iter().cycle().take(damaged_spring);
-                //        let end = ['.']
-                //            .into_iter()
-                //            .cycle()
-                //            .take(row.len() - start - damaged_spring);
-                //        rows_match(beginning.chain(middle).chain(end), row.chars())
-                //    })
-                //    .count()
-            }
-        }
-        n => {
-            let split = n / 2;
-            let lhs_damaged_springs = &damaged_springs[..split];
-            let rhs_damaged_springs = &damaged_springs[split..];
-            row.chars()
-                .enumerate()
-                .filter(|&(i, c)| c != '#')
-                .map(|(i, _)| {
-                    let lhs_row = &row[..i];
-                    let lhs_arrangements = count_arrangements(lhs_row, lhs_damaged_springs);
-                    if lhs_arrangements == 0 {
-                        return 0;
-                    }
-                    let rhs_row = &row[(i + 1)..];
-                    let rhs_arrangements = count_arrangements(rhs_row, rhs_damaged_springs);
-                    let answer = lhs_arrangements * rhs_arrangements;
-                    println!(
-                        "{:?} + {:?} = {:?} | {:?} + {:?} = {:?} -> {}",
-                        lhs_row,
-                        lhs_damaged_springs,
-                        lhs_arrangements,
-                        rhs_row,
-                        rhs_damaged_springs,
-                        rhs_arrangements,
-                        answer,
-                    );
-                    answer
-                })
-                .sum()
-        }
+                let right_arrangements = count_arrangements(right_row, right_damaged_springs);
+                left_arrangements * right_arrangements
+            })
+            .sum()
     }
 }
 
