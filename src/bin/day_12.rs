1adfbbcf7f9b1cdeaac90ad084cf76323c740643
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-15 23:41:12 -0700

    Starting to clean day 12 code

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -1,8 +1,4 @@
-#[allow(unused_imports)]
-use cached::proc_macro::cached;
-
-#[allow(unused_imports)]
-use regex::Regex;
+use std::iter;
 
 /// A row of springs, eiher '.' (operational), '#' (damaged), or '?' (unknown)
 type Row = String;
@@ -10,20 +6,14 @@ type Row = String;
 /// A list of contiguous damaged springs in a row
 type DamagedSprings = Vec<usize>;
 
-#[allow(unreachable_code)]
 fn main() {
-    //test_damaged_spring_regex();
-    //unimplemented!("test_damaged_spring_regex");
-
     // Parse the input, counting the number of matches per card
     let input = include_str!("../../puzzle_inputs/day_12.txt");
-    //let input = include_str!("../../puzzle_inputs/day_12_test_1.txt");
 
     // Solve 12a
     let puzzle = parse_input(input);
     let sol_12a: usize = solve(&puzzle);
     let correct_sol_12a: usize = 7622;
-    //let correct_sol_12a: usize = 21;
     println!("* 12a *");
     println!("My solution: {sol_12a}");
     println!("Correct solution: {correct_sol_12a}");
@@ -33,57 +23,28 @@ fn main() {
     let puzzle = increase_puzzle_size(puzzle);
     let sol_12b: usize = solve(&puzzle);
     let correct_sol_12b: usize = 4964259839627;
-    //let correct_sol_12b: usize = 525152;
     println!("* 12b *");
     println!("My solution: {sol_12b}");
     println!("Correct solution: {correct_sol_12b}");
     println!("Equal: {}\n", sol_12b == correct_sol_12b);
 }
 
-//#[allow(dead_code)]
-//fn test_damaged_spring_regex() {
-//    let _regex = damaged_spring_regex(1);
-//    let _regex = damaged_spring_regex(1);
-//    let _regex = damaged_spring_regex(1);
-//    let _regex = damaged_spring_regex(2);
-//    let _regex = damaged_spring_regex(2);
-//    let regex = damaged_spring_regex(3);
-//    let test_patterns = [".#.", ".#.?", "?#.", "?#?", "?#.?", "?.?#.?", "?.##.?", "#"];
-//    for pattern in test_patterns {
-//        let is_match = regex.is_match(pattern);
-//        println!("pattern: {}, is_match: {}", pattern, is_match);
-//    }
-//}
-
-#[allow(clippy::iter_skip_zero)]
 fn solve(puzzle: &[(Row, DamagedSprings)]) -> usize {
-    let mut total_arrangements = 0;
-    for (row, damaged_springs) in puzzle.iter().skip(0) {
-        println!("row: {:?}", row);
-        println!("damaged_springs: {:?}\n", damaged_springs);
-        let arrangements = count_arrangements(row, damaged_springs);
-        println!("arrangements: {}\n", arrangements);
-        total_arrangements += arrangements;
-        //panic!("Early exit");
-    }
-    //println!("total_arrangements: {}", total_arrangements);
-    total_arrangements
+    puzzle
+        .iter()
+        .map(|(row, damaged_springs)| count_arrangements(row, damaged_springs))
+        .sum()
 }
 
-#[allow(dead_code)]
 fn increase_puzzle_size(puzzle: Vec<(Row, DamagedSprings)>) -> Vec<(Row, DamagedSprings)> {
     puzzle
         .into_iter()
         .map(|(row, damaged_springs)| {
             let damaged_springs_len = damaged_springs.len();
-            let row = [
-                row.clone(),
-                row.clone(),
-                row.clone(),
-                row.clone(),
-                row.clone(),
-            ]
-            .join("?");
+            let row = iter::repeat(row.clone())
+                .take(5)
+                .collect::<Vec<_>>()
+                .join("?");
             let damaged_springs = damaged_springs
                 .into_iter()
                 .cycle()
@@ -254,21 +215,21 @@ fn matches_row(
     //true
 }
 
-#[cached]
-fn get_split_string(
-    damaged_spring: usize,
-    left_springs_empty: bool,
-    right_springs_empty: bool,
-) -> String {
-    let mut split_spring = "#".repeat(damaged_spring);
-    if !left_springs_empty {
-        split_spring.insert(0, '.');
-    }
-    if !right_springs_empty {
-        split_spring.push('.');
-    }
-    split_spring
-}
+//#[cached]
+//fn get_split_string(
+//    damaged_spring: usize,
+//    left_springs_empty: bool,
+//    right_springs_empty: bool,
+//) -> String {
+//    let mut split_spring = "#".repeat(damaged_spring);
+//    if !left_springs_empty {
+//        split_spring.insert(0, '.');
+//    }
+//    if !right_springs_empty {
+//        split_spring.push('.');
+//    }
+//    split_spring
+//}
 
 fn parse_input(input: &'static str) -> Vec<(Row, DamagedSprings)> {
     input
