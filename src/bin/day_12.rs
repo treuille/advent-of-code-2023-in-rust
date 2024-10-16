bb9069837e853e80662d002b6007c541a65a1a73
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-15 23:36:51 -0700

    Added in an acceleration that now solves in 3 seconds

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -22,8 +22,8 @@ fn main() {
     // Solve 12a
     let puzzle = parse_input(input);
     let sol_12a: usize = solve(&puzzle);
-    //let correct_sol_12a: usize = 7622;
-    let correct_sol_12a: usize = 21;
+    let correct_sol_12a: usize = 7622;
+    //let correct_sol_12a: usize = 21;
     println!("* 12a *");
     println!("My solution: {sol_12a}");
     println!("Correct solution: {correct_sol_12a}");
@@ -32,20 +32,14 @@ fn main() {
     // Solve 12b
     let puzzle = increase_puzzle_size(puzzle);
     let sol_12b: usize = solve(&puzzle);
-    let correct_sol_12b: usize = 525152;
+    let correct_sol_12b: usize = 4964259839627;
+    //let correct_sol_12b: usize = 525152;
     println!("* 12b *");
     println!("My solution: {sol_12b}");
     println!("Correct solution: {correct_sol_12b}");
     println!("Equal: {}\n", sol_12b == correct_sol_12b);
 }
 
-//#[cached]
-//fn damaged_spring_regex(damaged_spring: usize) -> Regex {
-//    let regex = format!(r"^[.?]*[#?]{{{damaged_spring}}}[.?]*$");
-//    println!("regex: {}", regex);
-//    return Regex::new(&regex).unwrap();
-//}
-//
 //#[allow(dead_code)]
 //fn test_damaged_spring_regex() {
 //    let _regex = damaged_spring_regex(1);
@@ -61,6 +55,7 @@ fn main() {
 //    }
 //}
 
+#[allow(clippy::iter_skip_zero)]
 fn solve(puzzle: &[(Row, DamagedSprings)]) -> usize {
     let mut total_arrangements = 0;
     for (row, damaged_springs) in puzzle.iter().skip(0) {
@@ -99,6 +94,7 @@ fn increase_puzzle_size(puzzle: Vec<(Row, DamagedSprings)>) -> Vec<(Row, Damaged
         .collect()
 }
 
+#[allow(dead_code)]
 fn rows_match(row_1: &str, row_2: &str) -> bool {
     //fn rows_match(
     //    row_1: impl IntoIterator<Item = char>,
@@ -147,36 +143,131 @@ fn count_arrangements(row: &str, damaged_springs: &[usize]) -> usize {
         //    row, damaged_springs
         //);
         let split = damaged_springs.len() / 2;
-        assert!(split >= 0 && split < damaged_springs.len());
+        assert!(split < damaged_springs.len());
         let left_damaged_springs = &damaged_springs[..split];
         let right_damaged_springs = &damaged_springs[(split + 1)..];
-        let mut split_spring = "#".repeat(damaged_springs[split]);
-        if !left_damaged_springs.is_empty() {
-            split_spring.insert(0, '.');
-        }
-        if !right_damaged_springs.is_empty() {
-            split_spring.push('.');
-        }
-        let split_spring_len = split_spring.len();
-        //println!(
-        //    "CASE C\nleft: {:?} split: {:?} right: {:?}\n",
-        //    left_damaged_springs, split_spring, right_damaged_springs
+
+        //let split_spring = get_split_string(
+        //    damaged_springs[split],
+        //    left_damaged_springs.is_empty(),
+        //    right_damaged_springs.is_empty(),
         //);
+
+        //let mut split_spring = String::with_capacity(damaged_springs[split] + 2);
+        let mut split_spring_len = damaged_springs[split];
+        if !left_damaged_springs.is_empty() {
+            //split_spring.push('.');
+            split_spring_len += 1;
+        }
+        //split_spring.extend(std::iter::repeat('#').take(damaged_springs[split]));
+        if !right_damaged_springs.is_empty() {
+            //split_spring.push('.');
+            split_spring_len += 1;
+        }
+        //let split_spring_len = split_spring.len();
         assert!(split_spring_len <= row.len());
-        (0..=row.len() - split_spring.len())
-            .filter(|&i| rows_match(&row[i..], &split_spring))
+
+        let mut pot_damaged: Vec<usize> = Vec::with_capacity(row.len());
+        pot_damaged.extend(row.chars().scan(0, |acc, c| {
+            if c != '.' {
+                *acc += 1;
+            }
+            Some(*acc)
+        }));
+        let max_pot_damaged = pot_damaged.last().unwrap();
+        let lds_sum: usize = left_damaged_springs.iter().sum();
+        let rds_sum: usize = right_damaged_springs.iter().sum();
+        (0..=row.len() - split_spring_len)
+            //.filter(|&i| {
+            //    ((i == 0) && (lds_sum == 0)) || ((i > 0) && (pot_damaged[i - 1] >= lds_sum))
+            //})
+            .filter(|&i| {
+                (i == 0)
+                    || ((i > 0)
+                        && (pot_damaged[i + split_spring_len - 1] - pot_damaged[i - 1]
+                            >= damaged_springs[split]))
+            })
+            .filter(|&i| (max_pot_damaged - pot_damaged[i + split_spring_len - 1]) >= rds_sum)
+            .filter(|&i| {
+                matches_row(
+                    &row[i..],
+                    damaged_springs[split],
+                    left_damaged_springs.is_empty(),
+                    right_damaged_springs.is_empty(),
+                )
+            })
             .map(|i| {
                 let left_row = &row[..i];
-                let right_row = &row[(i + split_spring_len)..];
                 let left_arrangements = count_arrangements(left_row, left_damaged_springs);
                 if left_arrangements == 0 {
                     return 0;
                 }
+                let right_row = &row[(i + split_spring_len)..];
                 let right_arrangements = count_arrangements(right_row, right_damaged_springs);
                 left_arrangements * right_arrangements
             })
             .sum()
     }
+}
+
+#[allow(dead_code, unreachable_code)]
+fn matches_row(
+    row: &str,
+    damaged_spring: usize,
+    left_springs_empty: bool,
+    right_springs_empty: bool,
+) -> bool {
+    let mut row = row.chars();
+    if !left_springs_empty && row.next() == Some('#') {
+        //panic!("Case A\nrow: {:?}\ndamaged_spring: {}\nleft_springs_empty: {}\nright_springs_empty: {}\n", row, damaged_spring, left_springs_empty, right_springs_empty);
+        return false;
+    }
+    for _ in 0..damaged_spring {
+        match row.next() {
+            Some('.') => {
+                //panic!("Case B\nrow: {:?}\ndamaged_spring: {}\nleft_springs_empty: {}\nright_springs_empty: {}\n", row, damaged_spring, left_springs_empty, right_springs_empty);
+                return false;
+            }
+            None => {
+                //panic!("Case C\nrow: {:?}\ndamaged_spring: {}\nleft_springs_empty: {}\nright_springs_empty: {}\n", row, damaged_spring, left_springs_empty, right_springs_empty);
+                return false;
+            }
+            _ => (),
+        }
+    }
+    if !right_springs_empty && row.next() == Some('#') {
+        //panic!("Case D\nrow: {:?}\ndamaged_spring: {}\nleft_springs_empty: {}\nright_springs_empty: {}\n", row, damaged_spring, left_springs_empty, right_springs_empty);
+        return false;
+    }
+    true
+    //if !left_springs_empty && row.chars().next() != Some('.') {
+    //    return false;
+    //}
+    //row = &row[1..];
+    //if row.chars().take(damaged_spring).any(|c| c == '.') {
+    //    return false;
+    //}
+    //row = &row[damaged_spring..];
+    //if !right_springs_empty && row.chars().next() != Some('.') {
+    //    return false;
+    //}
+    //true
+}
+
+#[cached]
+fn get_split_string(
+    damaged_spring: usize,
+    left_springs_empty: bool,
+    right_springs_empty: bool,
+) -> String {
+    let mut split_spring = "#".repeat(damaged_spring);
+    if !left_springs_empty {
+        split_spring.insert(0, '.');
+    }
+    if !right_springs_empty {
+        split_spring.push('.');
+    }
+    split_spring
 }
 
 fn parse_input(input: &'static str) -> Vec<(Row, DamagedSprings)> {
