2a79995f20e09dabef5ce2699bab3b6a7577e75c
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-18 18:51:28 -0700

    Really polished day 12

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -24,80 +24,69 @@ fn main() {
     println!("Equal: {}\n", sol_12b == correct_sol_12b);
 }
 
-fn solve(puzzle: &[(Vec<char>, Vec<usize>)]) -> usize {
+fn solve(puzzle: &[(String, Vec<usize>)]) -> usize {
     puzzle
         .iter()
         .map(|(row, damaged_springs)| count_arrangements(row.clone(), damaged_springs.clone()))
         .sum::<usize>()
 }
 
-fn increase_puzzle_size(puzzle: Vec<(Vec<char>, Vec<usize>)>) -> Vec<(Vec<char>, Vec<usize>)> {
+fn increase_puzzle_size(puzzle: Vec<(String, Vec<usize>)>) -> Vec<(String, Vec<usize>)> {
     puzzle
         .into_iter()
         .map(|(row, damaged_springs)| {
-            let row_len = row.len();
-            let damaged_spring_len = damaged_springs.len();
-            let row = row
-                .into_iter()
-                .chain(['?'])
-                .cycle()
-                .take(row_len * 5 + 4)
-                .collect();
-            let damaged_springs = damaged_springs
-                .into_iter()
-                .cycle()
-                .take(damaged_spring_len * 5)
-                .collect();
+            let row = iter::repeat(row).take(5).collect::<Vec<_>>().join("?");
+            let damaged_springs = iter::repeat(damaged_springs).take(5).flatten().collect();
             (row, damaged_springs)
         })
         .collect()
 }
 
 #[cached]
-fn count_arrangements(row: Vec<char>, damaged_springs: Vec<usize>) -> usize {
-    // This three-line acceleration decreases runtime by 30%, even with memoization
-    if row.iter().filter(|&&c| c != '.').count() < damaged_springs.iter().sum() {
+fn count_arrangements(row: String, damaged_springs: Vec<usize>) -> usize {
+    // This three-line acceleration decreases runtime by 30%
+    if row.chars().filter(|&c| c != '.').count() < damaged_springs.iter().sum() {
         return 0;
     }
-    match (row.first(), damaged_springs.first()) {
-        (Some(&'.'), _) => count_arrangements(row[1..].to_vec(), damaged_springs.to_vec()),
-        (Some(&'#'), Some(&first_damaged_spring)) if row.len() < first_damaged_spring => 0,
-        (Some(&'#'), Some(&first_damaged_spring))
-            if row[0..first_damaged_spring].iter().all(|&c| c != '.') =>
+    match (row.chars().next(), damaged_springs.first()) {
+        (Some('.'), _) => count_arrangements(row[1..].to_string(), damaged_springs.to_vec()),
+        (Some('#'), Some(&first_damaged_spring)) if row.len() < first_damaged_spring => 0,
+        (Some('#'), Some(&first_damaged_spring))
+            if row.chars().take(first_damaged_spring).all(|c| c != '.') =>
         {
             let row = &row[first_damaged_spring..];
             let damaged_springs = &damaged_springs[1..];
-            match row.first() {
-                Some(&'#') => 0,
-                Some(_) => count_arrangements(row[1..].to_vec(), damaged_springs.to_vec()),
-                None => count_arrangements(row.to_vec(), damaged_springs.to_vec()),
+            match row.chars().next() {
+                Some('#') => 0,
+                Some(_) => count_arrangements(row[1..].to_string(), damaged_springs.to_vec()),
+                None => count_arrangements(row.to_string(), damaged_springs.to_vec()),
             }
         }
-        (Some(&'#'), Some(_)) => 0,
-        (Some(&'#'), _) if row.iter().all(|&c| c != '#') => 1,
-        (Some(&'#'), _) => 0,
-        (Some(&'?'), _) => {
+        (Some('#'), Some(_)) => 0,
+        (Some('#'), _) if row.chars().all(|c| c != '#') => 1,
+        (Some('#'), _) => 0,
+        (Some('?'), _) => {
             count_arrangements(
-                iter::once('.').chain(row[1..].iter().copied()).collect(),
+                iter::once('.').chain(row.chars().skip(1)).collect(),
                 damaged_springs.to_vec(),
             ) + count_arrangements(
-                iter::once('#').chain(row[1..].iter().copied()).collect(),
+                iter::once('#').chain(row.chars().skip(1)).collect(),
                 damaged_springs.to_vec(),
             )
         }
         (None, Some(_)) => 0,
         (None, _) => 1,
-        (Some(&c), _) => unimplemented!("unexpected character: {}", c),
+        (Some(c), _) => unimplemented!("unexpected character: {}", c),
     }
 }
 
-fn parse_input(input: &'static str) -> Vec<(Vec<char>, Vec<usize>)> {
+fn parse_input(input: &'static str) -> Vec<(String, Vec<usize>)> {
     input
         .lines()
         .map(|line| {
             let (row, damaged_springs) = line.split_once(" ").unwrap();
             let damaged_springs = damaged_springs.split(",").flat_map(str::parse).collect();
-            (row.chars().collect(), damaged_springs)
+            (row.to_string(), damaged_springs)
         })
         .collect()
 }
