68836f29c947c123e43901d85c6fa76de93e3179
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-26 22:56:23 -0700

    Started working on day 14

---
+++ src/bin/day_14.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // Parse the input, counting the number of matches per card
+    //let input = include_str!("../../puzzle_inputs/day_08_test.txt");
+    //println!("input len: {}", input.len());
+    //println!("input:\n{}", input);
+
+    // Solve 14
+    let sol_14: usize = 12;
+    let correct_sol_14: usize = 32;
+    println!("* 14 *");
+    println!("My solution: {sol_14}");
+    println!("Correct solution: {correct_sol_14}");
+    println!("Equal: {}\n", sol_14 == correct_sol_14);
+
+    // Solve 14b
+    let sol_14b: usize = 56;
+    let correct_sol_14b: usize = 79;
+    println!("* 14b *");
+    println!("My solution: {sol_14b}");
+    println!("Correct solution: {correct_sol_14b}");
+    println!("Equal: {}\n", sol_14b == correct_sol_14b);
+}
