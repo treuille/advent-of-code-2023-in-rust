b2cdbfb6d14fab9a2ead31a5cb51f3fbcc527111
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-28 14:58:58 -0700

    Solved 15a

--- src/bin/day_15.rs
+++ src/bin/day_15.rs
@@ -1,22 +1,42 @@
 fn main() {
     //// Parse the input, counting the number of matches per card
-    //let input = include_str!("../../puzzle_inputs/day_08_test.txt");
-    //println!("input len: {}", input.len());
-    //println!("input:\n{}", input);
+    let input = include_str!("../../puzzle_inputs/day_15.txt").trim();
+    //let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+    let answer: u64 = input
+        .split(",")
+        .map(|s| {
+            println!("{} {}", s, hash(s));
+            s
+        })
+        .map(hash)
+        .sum();
+    println!("answer: {}", answer);
 
-    // Solve 15a
-    let sol_15a: usize = 12;
-    let correct_sol_15a: usize = 32;
-    println!("* 15a *");
-    println!("My solution: {sol_15a}");
-    println!("Correct solution: {correct_sol_15a}");
-    println!("Equal: {}\n", sol_15a == correct_sol_15a);
+    //// Get the ascii code for c
+    //let c = 'H';
+    //println!("c: {}", c);
+    //println!("ascii(c): {}", c as u64);
+    //println!("hash: {}", hash("HASH"));
 
-    // Solve 15b
-    let sol_15b: usize = 56;
-    let correct_sol_15b: usize = 79;
-    println!("* 15b *");
-    println!("My solution: {sol_15b}");
-    println!("Correct solution: {correct_sol_15b}");
-    println!("Equal: {}\n", sol_15b == correct_sol_15b);
+    //// Solve 15a
+    //let sol_15a: usize = 12;
+    //let correct_sol_15a: usize = 32;
+    //println!("* 15a *");
+    //println!("My solution: {sol_15a}");
+    //println!("Correct solution: {correct_sol_15a}");
+    //println!("Equal: {}\n", sol_15a == correct_sol_15a);
+    //
+    //// Solve 15b
+    //let sol_15b: usize = 56;
+    //let correct_sol_15b: usize = 79;
+    //println!("* 15b *");
+    //println!("My solution: {sol_15b}");
+    //println!("Correct solution: {correct_sol_15b}");
+    //println!("Equal: {}\n", sol_15b == correct_sol_15b);
+}
+
+fn hash(s: &str) -> u64 {
+    s.chars().fold(0u64, |current_value, c| {
+        (current_value + (c as u64)) * 17 % 256
+    })
 }
