d0df2a00f7b5078993651a0c3962893504e07eaa
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-10-28 15:16:47 -0700

    Starting to work on part b

--- src/bin/day_15.rs
+++ src/bin/day_15.rs
@@ -1,16 +1,18 @@
+use std::array;
+
 fn main() {
     //// Parse the input, counting the number of matches per card
     let input = include_str!("../../puzzle_inputs/day_15.txt").trim();
+
     //let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
-    let answer: u64 = input
-        .split(",")
-        .map(|s| {
-            println!("{} {}", s, hash(s));
-            s
-        })
-        .map(hash)
-        .sum();
+
+    // Part A
+    let answer: u64 = input.split(",").map(hash).sum();
     println!("answer: {}", answer);
+
+    // Part B
+    let boxes: [Vec<(&str, u64)>; 256] = array::from_fn(|_| Vec::new());
+    dbg!(boxes);
 
     //// Get the ascii code for c
     //let c = 'H';
